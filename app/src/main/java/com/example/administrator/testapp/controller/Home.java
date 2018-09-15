f8453aa6a47fa9dde89040ec1b46a6358ae92fe9
wj <1165669057@qq.com>
2018-09-15 21:31:02 +0800

    改了一些东西

--- app/src/main/java/com/example/administrator/testapp/controller/Home.java
+++ app/src/main/java/com/example/administrator/testapp/controller/Home.java
@@ -3,6 +3,9 @@ package com.example.administrator.testapp.controller;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,17 +16,18 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class Home extends AppCompatActivity implements View.OnClickListener{
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.myMainDrawerLayout)
+    DrawerLayout myMainDrawerLayout;
+    @BindView(R.id.myMainViewPage)
+    ViewPager myMainViewPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-        fab.setOnClickListener(this);
     }
     @Override
     public void onClick(View view) {
